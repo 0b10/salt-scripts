5b7f67f87c57cc718cf52ef73d1d6c17c62989cf
0b10 <0b10@pm.me>
2019-11-27 03:17:14 +0000

    Build firefox config

--- apps/firefox/pull-config.sh
+++ apps/firefox/pull-config.sh
@@ -3,17 +3,21 @@
 __STORE="${HOME}/git-test";
 __REPO="${__STORE}/user.js";
 __REMOTE="https://github.com/pyllyukko/user.js.git";
+__MAKE_TARGET="systemwide_user.js";
+__DIST="systemwide_user.js";
 
 [[ `whoami` == "root" ]] && echo "pulling firefox config as root, probably not a good idea" && return 1;
 
-if [[ ! -d $__REPO ]]; then {
-  (
-    mkdir -p $__STORE && \
-    cd $__STORE && \
-    git clone $__REMOTE;
-  )
-}
+if [[ ! -d $__REPO ]]; then
+    (
+        mkdir -p $__STORE && \
+        cd $__STORE && \
+        git clone $__REMOTE;
+    )
+fi
 
-
-
-
+(
+    echo "pulling..."
+    cd $__REPO && git pull && \
+    make $__MAKE_TARGET
+)
