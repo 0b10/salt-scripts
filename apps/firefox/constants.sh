ea3d567bb1f36f09917659c5b2d03dcd95cd3ad3
0b10 <0b10@pm.me>
2019-11-27 22:31:13 +0000

    Use constants file
       ...to share variables between scripts

---
+++ apps/firefox/constants.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+__PULL_STORE="${HOME}/.pull/firefox/config";
+__REPO_STORE="${HOME}/git-test";
+__REPO="${__REPO_STORE}/user.js";
+__REMOTE="https://github.com/pyllyukko/user.js.git";
+__MAKE_TARGET="systemwide_user.js";
+__DIST="systemwide_user.js";
--- apps/firefox/pull-config.sh
+++ apps/firefox/pull-config.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-__PULL_STORE="${HOME}/.pull/firefox/config"
-__REPO_STORE="${HOME}/git-test";
-__REPO="${__REPO_STORE}/user.js";
-__REMOTE="https://github.com/pyllyukko/user.js.git";
-__MAKE_TARGET="systemwide_user.js";
-__DIST="systemwide_user.js";
+source "$(dirname $0)/constants.sh";
 
 echo "Pulling a new firefox syspref..."
 
