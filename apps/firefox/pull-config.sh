a72e0aec1545095ebe6c95d7f6e05d3facad8aeb
0b10 <0b10@pm.me>
2019-11-27 03:08:56 +0000

    Make logic for pulling firefox config repo

--- apps/firefox/pull-config.sh
+++ apps/firefox/pull-config.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-__STORE="${HOME}/git";
+__STORE="${HOME}/git-test";
 __REPO="${__STORE}/user.js";
 __REMOTE="https://github.com/pyllyukko/user.js.git";
 
 [[ `whoami` == "root" ]] && echo "pulling firefox config as root, probably not a good idea" && return 1;
 
-[[ -d $__REPO ]] || mkdir -p $__STORE;
+if [[ ! -d $__REPO ]]; then {
+  (
+    mkdir -p $__STORE && \
+    cd $__STORE && \
+    git clone $__REMOTE;
+  )
+}
+
+
 
 
